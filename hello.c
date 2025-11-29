1b29a9b324abc3d6675371340a47ca09fe531d7d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-11-29 15:59:36 +0000

    add code for converting celsius to fahr

--- hello.c
+++ hello.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-/* print Fahrenheit-Celsius table
-for fahr = 0, 20, ..., 300 */
+
 main() {
   float fahr, celsius;
   float lower, upper, step;
-  lower = 0;
-  upper = 300;
-  step = 20; /* lower limit of temperature scale */
-  /* upper limit */
-  /* step size */
+  char *fahrTableHeading, *celsiusTableHeading;
+  lower = 0;   /* lower limit of temperature scale */
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
+
+  fahrTableHeading = "Fahrenheit";
+  celsiusTableHeading = "Celsius";
+
+  /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
   fahr = lower;
+  printf("%s\t%s\n", fahrTableHeading, celsiusTableHeading);
   while (fahr <= upper) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%.1f\t%1.1f\n", fahr, celsius);
+    printf("%10.0f\t%7.1f\n", fahr, celsius);
     fahr = fahr + step;
+  }
+  printf("\n");
+
+  /* print Celsius-Fahrenheit table for celsius = 0, 20, ..., 300 */
+  celsius = lower;
+  printf("%s\t%s\n", celsiusTableHeading, fahrTableHeading);
+  while (celsius <= upper) {
+    fahr = ((9.0 * celsius) / 5.0) + 32.0;
+    printf("%7.0f\t%10.1f\n", celsius, fahr);
+    celsius = celsius + step;
   }
 }
