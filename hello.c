842efbe50cd3a7bfa2be928cc5de4c8d5614aaa8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-12-07 14:59:11 +0000

    input to output 1st version

--- hello.c
+++ hello.c
@@ -1,42 +1,11 @@
 #include <stdio.h>
 
-#define UPPER 300 /* upper limit of temperature scale */
-#define LOWER 0   /* lower limit */
-#define STEP 20   /* step size */
-
 main() {
-  int fahr;
+  int c;
+  c = getchar();
 
-  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
-    printf("%3d\t%6.1f\n", fahr, ((5.0 / 9.0) * (fahr - 32.0)));
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
   }
-
-  //   float fahr, celsius;
-  //   float lower, upper, step;
-  //   char *fahrTableHeading, *celsiusTableHeading;
-  //   lower = 0;   /* lower limit of temperature scale */
-  //   upper = 300; /* upper limit */
-  //   step = 20;   /* step size */
-
-  //   fahrTableHeading = "Fahrenheit";
-  //   celsiusTableHeading = "Celsius";
-
-  //   /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
-  //   fahr = lower;
-  //   printf("%s\t%s\n", fahrTableHeading, celsiusTableHeading);
-  //   while (fahr <= upper) {
-  //     celsius = (5.0 / 9.0) * (fahr - 32.0);
-  //     printf("%10.0f\t%7.1f\n", fahr, celsius);
-  //     fahr = fahr + step;
-  //   }
-  //   printf("\n");
-
-  //   /* print Celsius-Fahrenheit table for celsius = 0, 20, ..., 300 */
-  //   celsius = lower;
-  //   printf("%s\t%s\n", celsiusTableHeading, fahrTableHeading);
-  //   while (celsius <= upper) {
-  //     fahr = ((9.0 * celsius) / 5.0) + 32.0;
-  //     printf("%7.0f\t%10.1f\n", celsius, fahr);
-  //     celsius = celsius + step;
-  //   }
 }
