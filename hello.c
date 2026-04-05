98d91e85789af188ce3a67c144144105388a2d06
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-05 03:57:59 +0100

    exercise 1-8

--- hello.c
+++ hello.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 
-main() { printf("%d\n", EOF); }
+main() {
+  int nb = 0;
+  int nt = 0;
+  int nl = 0;
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      ++nb;
+    }
+    if (c == '\t') {
+      ++nt;
+    }
+    if (c == '\n') {
+      ++nl;
+    }
+  }
+  printf("Number of blanks is: %d\n", nb);
+  printf("Number of tabs is: %d\n", nt);
+  printf("Number of lines is: %d\n", nl);
+}
