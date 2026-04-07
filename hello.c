6146949ed39864c51333eeaa7bbd5a17c371f2c6
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-07 17:22:55 +0100

    exercise 1-14

--- hello.c
+++ hello.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
-#define IS_NON_WORD 1
-#define IS_WORD 0
+// #define IS_NON_WORD 1
+// #define IS_WORD 0
 
 main() {
-  int c, i, j, k;
-  int word_state = IS_NON_WORD;
-  int wlengthcounter = 0;
-  int allowed_words = 10;
-  char words[allowed_words];
+  int c, i, j;
+  int arraylength = 12;
+  int index_white = arraylength - 2;
+  int index_other = arraylength - 1;
+  int ndigit[arraylength];
 
-  for (i = 0; i < allowed_words; ++i) {
-    words[i] = 0;
-    printf("0");
+  for (i = 0; i < arraylength; ++i) {
+    ndigit[i] = 0;
   }
-  printf("\n");
 
-  while ((c = getchar()) != EOF && wlengthcounter < allowed_words) {
-    if (c != ' ' && c != '\n' && c != '\t') {
-      ++words[wlengthcounter];
-      word_state = IS_WORD;
-    } else if (word_state == IS_WORD) {
-      ++wlengthcounter;
-      word_state = IS_NON_WORD;
+  while ((c = getchar()) != EOF) {
+    if (c >= '0' && c <= '9') {
+      ++ndigit[c - '0'];
+    } else if (c == ' ' || c == '\n' || c == '\t') {
+      ++ndigit[index_white];
+    } else {
+      ++ndigit[index_other];
     }
   }
 
-  for (j = 0; j < allowed_words; ++j) {
-    if (words[j] == 0) {
-      printf("No words\n");
+  for (i = 0; i < arraylength; ++i) {
+    if (ndigit[i] == 0) {
+      printf("No input for this character\n");
     } else {
-      for (k = 0; k < words[j]; ++k) {
+      if (i == index_white) {
+        printf("White space:\t");
+      } else if (i == index_other) {
+        printf("Other char:\t");
+      } else {
+        printf("%d:\t", i);
+      }
+
+      for (j = 0; j < ndigit[i]; ++j) {
         printf("|");
       }
       printf("\n");
