3c3b30feaeca2d45339721fc6f9d03116b7b5984
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-08 13:24:03 +0100

    add first code snippet for section 1.9

--- hello.c
+++ hello.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
-/* print Fahrenheit-Celsius table
-for fahr = 0, 20, ..., 300; floating-point version */
+#define MAXLINE 1000 /* maximum input line length */
 
-float fahr_to_cels(float fahr);
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
-int main() {
-  float fahr, celsius;
-  float lower, upper, step;
-  lower = 0;   /* lower limit of temperatuire scale */
-  upper = 300; /* upper limit */
-  step = 20;   /* step size */
+/* print the longest input line */
+main() {
+  int len;               /* current line length */
+  int max;               /* maximum length seen so far */
+  char line[MAXLINE];    /* current input line */
+  char longest[MAXLINE]; /* longest line saved here */
+  max = 0;
+  while ((len = getline(line, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
 
-  fahr = lower;
-  while (fahr <= upper) {
-    celsius = fahr_to_cels(fahr);
-    printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+  /* there was a line */
+  if (max > 0) {
+    printf("%s", longest);
   }
 
   return 0;
 }
 
-float fahr_to_cels(float fahr) {
-  float celsius = (5.0 / 9.0) * (fahr - 32.0);
-  return celsius;
+/* getline: read a line into s, return length */
+int getline(char s[], int lim) {
+  int c, i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+  return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]) {
+  int i;
+  i = 0;
+  
+  while ((to[i] = from[i]) != '\0') {
+    ++i;
+  }
 }
