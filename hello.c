4926c5944039e0604c603c3fa663fbe467a84c04
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-08 17:07:29 +0100

    change function name to get_line

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 #define MAXLINE 1000 /* maximum input line length */
 
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print the longest input line */
@@ -11,7 +12,7 @@ main() {
   char line[MAXLINE];    /* current input line */
   char longest[MAXLINE]; /* longest line saved here */
   max = 0;
-  while ((len = getline(line, MAXLINE)) > 0) {
+  while ((len = get_line(line, MAXLINE)) > 0) {
     if (len > max) {
       max = len;
       copy(longest, line);
@@ -26,19 +27,19 @@ main() {
   return 0;
 }
 
-/* getline: read a line into s, return length */
-int getline(char s[], int lim) {
+/* get_line: read a line into s, return length */
+int get_line(char line[], int maxline) {
   int c, i;
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+  for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    line[i] = c;
   }
 
   if (c == '\n') {
-    s[i] = c;
+    line[i] = c;
     ++i;
   }
 
-  s[i] = '\0';
+  line[i] = '\0';
   return i;
 }
 
@@ -46,7 +47,7 @@ int getline(char s[], int lim) {
 void copy(char to[], char from[]) {
   int i;
   i = 0;
-  
+
   while ((to[i] = from[i]) != '\0') {
     ++i;
   }
