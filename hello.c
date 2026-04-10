d2645bde92bca57004d70f89e981cd7ff3591b24
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-10 16:07:04 +0100

    exercise 1-18

--- hello.c
+++ hello.c
@@ -4,40 +4,36 @@
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
+int clean_line(int start_length, char start_line[], char end_line[]);
 
-/* print the longest input line */
-main() {
-  int len;               /* current line length */
-  int max;               /* maximum length seen so far */
-  char line[MAXLINE];    /* current input line */
-  char longest[MAXLINE]; /* longest line saved here */
-  int i;
+/* print only non-empty lines */
+int main(void) {
+  int len;                  /* current line length */
+  int clean_line_length;    /* length of line after cleaning */
+  char line_input[MAXLINE]; /* current input line */
+  char line_clean[MAXLINE]; /* current cleaned line saved here */
 
-  max = 0;
-  while ((len = get_line(line, MAXLINE)) > 0) {
-    char s;
+  while ((len = get_line(line_input, MAXLINE)) > 0) {
     printf("Length of line input: %d\n", len);
-    if (len >= MAXLINE) {
-      printf("The line input is longer than the line array:\n");
-      printf("The line input is obviously also greater than 80 characters:\n");
-      for (i = 0; i < (MAXLINE - 1); ++i) {
-        s = line[i];
-        printf("%c", s);
-      }
-      printf("\n");
-    } else if (len > 80) {
-      printf("The line input is the same as or shorter than the linearray and also greater than 80 characters:\n");
-      for (i = 0; i < len; ++i) {
-        s = line[i];
-        printf("%c", s);
-      }
+    clean_line_length = clean_line(len, line_input, line_clean);
+    printf("Length of clean line: %d\n", clean_line_length);
+    if (clean_line_length == 0) {
+      printf("This is not a valid line to print because the clean version is empty\n");
+    } else if (clean_line_length == 1 && line_clean[clean_line_length - 1] == '\n') {
+      printf("This is not a valid line to print because the clean version contains only 1 \\n\n");
+    } else if (clean_line_length == 1 && line_clean[clean_line_length - 1] != '\n') {
+      printf("This is a valid line to print because the cleaned version is not empty\n");
+      printf("%s\n", line_clean);
+    } else {
+      printf("This is a valid line to print because the cleaned version is not empty\n");
+      printf("%s\n", line_clean);
     }
   }
 
   return 0;
 }
 
-/* get_line: read a line into s, return length */
+/* get_line: read a line into s, return length (excluding null terminator) */
 int get_line(char line[], int maxline) {
   int i;
   char c;
@@ -57,11 +53,11 @@ int get_line(char line[], int maxline) {
 
   if (i < maxline) {
     line[i] = '\0';
+    return i;
   } else {
     line[maxline - 1] = '\0';
+    return (maxline - 1);
   }
-
-  return i;
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
@@ -71,5 +67,47 @@ void copy(char to[], char from[]) {
 
   while ((to[i] = from[i]) != '\0') {
     ++i;
+  }
+}
+
+/* clean_line: remove trailing tabs and spaces from line input. 'start_length' excludes null terminator */
+int clean_line(int start_line_length, char start_line[], char end_line[]) {
+  int i;
+
+  if (start_line[start_line_length - 1] == '\n') {
+    i = start_line_length - 2;
+    while (i >= 0 && ((start_line[i] == ' ') || (start_line[i] == '\t'))) {
+      --i;
+    }
+
+    if (i == -1) {
+      end_line[0] = '\n';
+      end_line[1] = '\0';
+      return i + 2;
+    } else {
+      int j;
+      for (j = 0; j < i + 2; ++j) {
+        end_line[j] = start_line[j];
+      }
+      end_line[j] = '\0';
+      return j;
+    }
+  } else {
+    i = start_line_length - 1;
+    while (i >= 0 && ((start_line[i] == ' ') || (start_line[i] == '\t'))) {
+      --i;
+    }
+
+    if (i == -1) {
+      end_line[0] = '\0';
+      return i + 1;
+    } else {
+      int j;
+      for (j = 0; j < i + 1; ++j) {
+        end_line[j] = start_line[j];
+      }
+      end_line[j] = '\0';
+      return j;
+    }
   }
 }
