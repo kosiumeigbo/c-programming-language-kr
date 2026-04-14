020a8f350311f4b0df2ecf94cb6b072b146c8e8d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-04-14 21:01:31 +0100

    exercise 2-1 (excluding float and double)

--- hello.c
+++ hello.c
@@ -1,21 +1,33 @@
 #include <limits.h>
 #include <stdio.h>
 
-long unsigned_max_value(char type, char qual);
+long max_value(char type, char qual);
 
 int main() {
-  printf("for unsigned char: 0 to %ld\n", (unsigned_max_value('c', 'U') - 1));
-  printf("for unsigned short: 0 to %ld\n", (unsigned_max_value('s', 'U') - 1));
-  printf("for unsigned int: 0 to %ld\n", (unsigned_max_value('i', 'U') - 1));
-  printf("for unsigned long: 0 to %ld\n", (unsigned_max_value('l', 'U') - 1));
-  printf("for signed char: -%ld to %ld\n", (unsigned_max_value('c', 'S')), (unsigned_max_value('c', 'S') - 1));
-  printf("for signed short: -%ld to %ld\n", (unsigned_max_value('s', 'S')), (unsigned_max_value('s', 'S') - 1));
-  printf("for signed int: -%ld to %ld\n", (unsigned_max_value('i', 'S')), (unsigned_max_value('i', 'S') - 1));
-  printf("for signed long: -%ld to %ld\n", (unsigned_max_value('l', 'S')), (unsigned_max_value('l', 'S') - 1));
+
+  printf("Based on function:\n");
+  printf("for unsigned char: 0 to %lu\n", (max_value('c', 'U') - 1));
+  printf("for unsigned short: 0 to %lu\n", (max_value('s', 'U') - 1));
+  printf("for unsigned int: 0 to %lu\n", (max_value('i', 'U') - 1));
+  printf("for unsigned long: 0 to %lu\n", (max_value('l', 'U') - 1));
+  printf("for signed char: -%lu to %ld\n", (max_value('c', 'S')), (max_value('c', 'S') - 1));
+  printf("for signed short: -%lu to %ld\n", (max_value('s', 'S')), (max_value('s', 'S') - 1));
+  printf("for signed int: -%lu to %ld\n", (max_value('i', 'S')), (max_value('i', 'S') - 1));
+  printf("for signed long: -%lu to %ld\n", (max_value('l', 'S')), (max_value('l', 'S') - 1));
+  printf("\n");
+  printf("Based on limits.h\n");
+  printf("for unsigned char: 0 to %u\n", UCHAR_MAX);
+  printf("for unsigned short: 0 to %u\n", USHRT_MAX);
+  printf("for unsigned int: 0 to %u\n", UINT_MAX);
+  printf("for unsigned long: 0 to %lu\n", ULONG_MAX);
+  printf("for signed char: %d to %d\n", SCHAR_MIN, SCHAR_MAX);
+  printf("for signed short: %d to %d\n", SHRT_MIN, SHRT_MAX);
+  printf("for signed int: %d to %d\n", INT_MIN, INT_MAX);
+  printf("for signed long: %ld to %ld\n", LONG_MIN, LONG_MAX);
   return 0;
 }
 
-long unsigned_max_value(char type, char qual) {
+long max_value(char type, char qual) {
   int pow;
   int i;
   long base = 2;
@@ -29,7 +41,7 @@ long unsigned_max_value(char type, char qual) {
     } else if (type == 'i') { /* int */
       pow = 32;
     } else if (type == 'l') { /* long */
-      pow = 32;
+      pow = 64;
     } else {
       pow = 0;
     }
@@ -41,7 +53,7 @@ long unsigned_max_value(char type, char qual) {
     } else if (type == 'i') { /* int */
       pow = 31;
     } else if (type == 'l') { /* long */
-      pow = 31;
+      pow = 63;
     } else {
       pow = 0;
     }
